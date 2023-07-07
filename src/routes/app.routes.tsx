3c0440db96b84ad0894757d5c59c0e88af124b5a
Paulo <pauloh16.pdr@gmail.com>
2023-07-07 01:49:15 -0300

    feat: criar tela de signup

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,7 +3,7 @@ import { VStack } from 'native-base';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { Home, SignIn } from '@screens/index';
+import { Home, SignIn, SignUp } from '@screens/index';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -11,11 +11,12 @@ const AppRoutes: React.FC = () => {
 	return (
 		<VStack flex={1} bg='gray.700'>
 			<Navigator
-				initialRouteName='signIn'
+				initialRouteName='signUp'
 				screenOptions={{ headerShown: false }}
 			>
 				<Screen name='home' component={Home} />
 				<Screen name='signIn' component={SignIn} />
+				<Screen name='signUp' component={SignUp} />
 			</Navigator>
 		</VStack>
 	);
---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { ScrollView, VStack, Image, Center, Text, Heading } from 'native-base';
+
+import LogoSvg from '@icons/logo.svg';
+import BackgroundImage from '@images/background.png';
+
+import { Button, Input } from '@components/index';
+
+const SignUp: React.FC = () => {
+	return (
+		<ScrollView
+			contentContainerStyle={{ flexGrow: 1 }}
+			showsVerticalScrollIndicator={false}
+			bg='gray.700'
+		>
+			<VStack flex={1} bg='gray.700' px={10} pb={16}>
+				<Image
+					source={BackgroundImage}
+					alt='Pessoas se exercitando.'
+					resizeMode='contain'
+					position='absolute'
+				/>
+
+				<Center my={24}>
+					<LogoSvg />
+
+					<Text color='gray.100' fontSize='md'>
+						Treine sua mente e o seu corpo
+					</Text>
+				</Center>
+
+				<Center>
+					<Heading color='gray.100' fontSize='xl' mb={6} fontFamily='heading'>
+						Crie sua conta
+					</Heading>
+				</Center>
+
+				<Center mb={24}>
+					<Input placeholder='Nome' />
+
+					<Input
+						placeholder='E-mail'
+						keyboardType='email-address'
+						autoCapitalize='none'
+					/>
+
+					<Input placeholder='Senha' secureTextEntry />
+
+					<Button title='Criar e acessar' />
+				</Center>
+
+				<Button title='Voltar para login' variant='outline' />
+			</VStack>
+		</ScrollView>
+	);
+};
+
+export default SignUp;
