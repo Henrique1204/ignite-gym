6114bc87f367a100afcbd7f337c0c8eb56180571
Paulo <pauloh16.pdr@gmail.com>
2023-07-07 01:23:45 -0300

    feat: ajustar inputs na tela de signin

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -8,7 +8,7 @@ import { Input } from '@components/index';
 
 const SignIn: React.FC = () => {
 	return (
-		<VStack flex={1} bg='gray.700'>
+		<VStack flex={1} bg='gray.700' px={10}>
 			<Image
 				source={BackgroundImage}
 				alt='Pessoas se exercitando.'
@@ -26,8 +26,13 @@ const SignIn: React.FC = () => {
 				</Heading>
 			</Center>
 
-			<Input placeholder='E-mail' />
-			<Input placeholder='Senha' />
+			<Input
+				placeholder='E-mail'
+				keyboardType='email-address'
+				autoCapitalize='none'
+			/>
+
+			<Input placeholder='Senha' secureTextEntry />
 		</VStack>
 	);
 };
