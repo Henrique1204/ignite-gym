99a5eb52428e3032f99fdcc9566be0a283b9d1f7
Paulo <pauloh16.pdr@gmail.com>
2023-07-08 15:28:13 -0300

    feat: refatorar rotas do site para bottom tabs

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
-import { VStack } from 'native-base';
 
-import {
-	createNativeStackNavigator,
-	NativeStackNavigationProp,
-} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import { Exercise, History, Home, Profile } from '@screens/index';
 
-type IAppRoutes = {
-	exercise: undefined;
-	history: undefined;
-	home: undefined;
-	profile: undefined;
-};
-
-export type IAppNavigatorRoutesProps = NativeStackNavigationProp<IAppRoutes>;
-
-const { Navigator, Screen } = createNativeStackNavigator<IAppRoutes>();
+const { Navigator, Screen } = createBottomTabNavigator();
 
 const AppRoutes: React.FC = () => {
 	return (
-		<VStack flex={1} bg='gray.700'>
-			<Navigator initialRouteName='home' screenOptions={{ headerShown: false }}>
-				<Screen name='exercise' component={Exercise} />
-				<Screen name='history' component={History} />
-				<Screen name='home' component={Home} />
-				<Screen name='profile' component={Profile} />
-			</Navigator>
-		</VStack>
+		<Navigator initialRouteName='home' screenOptions={{ headerShown: false }}>
+			<Screen name='exercise' component={Exercise} />
+			<Screen name='history' component={History} />
+			<Screen name='home' component={Home} />
+			<Screen name='profile' component={Profile} />
+		</Navigator>
 	);
 };
 
