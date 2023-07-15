3ce924913dcb142e9f9c70f10add47acf0d5e169
Paulo <pauloh16.pdr@gmail.com>
2023-07-15 15:51:18 -0300

    feat: criar cabecalho da lista de exercicios

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { FlatList, VStack } from 'native-base';
+import { FlatList, Heading, HStack, Text, VStack } from 'native-base';
 
 import { Group, HomeHeader } from '@components/index';
 
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
 	const handleChangeGroup = (name: string) => () => setGroupActive(name);
 
 	return (
-		<VStack>
+		<VStack flex={1}>
 			<HomeHeader />
 
 			<FlatList
@@ -33,6 +33,18 @@ const Home: React.FC = () => {
 				my={10}
 				maxH={10}
 			/>
+
+			<VStack flex={1} px={8}>
+				<HStack justifyContent='space-between' mb={5}>
+					<Heading color='gray.200' fontSize='md'>
+						Exercícios
+					</Heading>
+
+					<Text color='gray.200' fontSize='sm'>
+						4
+					</Text>
+				</HStack>
+			</VStack>
 		</VStack>
 	);
 };
