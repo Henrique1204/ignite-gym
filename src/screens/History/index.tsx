d77535918d50069aebf27c62f5c81330a0acdce9
Paulo <pauloh16.pdr@gmail.com>
2023-07-18 21:58:08 -0300

    feat: adicionar exemplo de sectionList

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,17 +1,41 @@
 import React from 'react';
 
-import { VStack } from 'native-base';
+import { Heading, SectionList, VStack } from 'native-base';
 
 import { ScreenHeader, HistoryCard } from '@components/index';
 
+interface IExerciseType {
+	title: string;
+	data: string[];
+}
+
 const History: React.FC = () => {
+	const [exercise, setExercise] = React.useState<IExerciseType[]>([
+		{
+			title: '26.08.22',
+			data: ['Puxada frontal', 'Remada unilateral'],
+		},
+		{
+			title: '27.08.22',
+			data: ['Puxada frontal'],
+		},
+	]);
+
 	return (
 		<VStack>
 			<ScreenHeader title='Histórico de Exercícios' />
 
-			<HistoryCard />
-			<HistoryCard />
-			<HistoryCard />
+			<SectionList
+				sections={exercise}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => <HistoryCard />}
+				renderSectionHeader={({ section }) => (
+					<Heading color='gray.200' fontSize='md' mt={10} mb={3}>
+						{section.title}
+					</Heading>
+				)}
+				px={8}
+			/>
 		</VStack>
 	);
 };
