54b19936641143f281f99c782b816790cb7156bb
Paulo <pauloh16.pdr@gmail.com>
2023-08-14 06:23:33 -0300

    feat: adicionar controle de formulario para signin

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { ScrollView, VStack, Image, Center, Text, Heading } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
 
+import { Controller, useForm } from 'react-hook-form';
+
 import { IAuthNavigatorRoutesProps } from '@routes/auth.routes';
 
 import { Button, Input } from '@components/index';
@@ -9,10 +11,25 @@ import { Button, Input } from '@components/index';
 import LogoSvg from '@icons/logo.svg';
 import BackgroundImage from '@images/background.png';
 
+type IFormDataProps = {
+	email: string;
+	password: string;
+};
+
 const SignIn: React.FC = () => {
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IFormDataProps>();
+
 	const { navigate } = useNavigation<IAuthNavigatorRoutesProps>();
 
 	const goToSignUp = () => navigate('signUp');
+
+	const handleSignIn = handleSubmit(async (body) => {
+		console.log(body);
+	});
 
 	return (
 		<ScrollView
@@ -43,15 +60,38 @@ const SignIn: React.FC = () => {
 				</Center>
 
 				<Center>
-					<Input
-						placeholder='E-mail'
-						keyboardType='email-address'
-						autoCapitalize='none'
+					<Controller
+						name='email'
+						control={control}
+						rules={{ required: 'Informa o e-mail.' }}
+						render={({ field: { value, onChange } }) => (
+							<Input
+								placeholder='E-mail'
+								keyboardType='email-address'
+								autoCapitalize='none'
+								value={value}
+								onChange={onChange}
+								errorMessage={errors.email?.message}
+							/>
+						)}
 					/>
 
-					<Input placeholder='Senha' secureTextEntry />
+					<Controller
+						name='password'
+						control={control}
+						rules={{ required: 'Informa a senha.' }}
+						render={({ field: { value, onChange } }) => (
+							<Input
+								placeholder='Senha'
+								secureTextEntry
+								value={value}
+								onChange={onChange}
+								errorMessage={errors.password?.message}
+							/>
+						)}
+					/>
 
-					<Button title='Acessar' />
+					<Button title='Acessar' onPress={handleSignIn} />
 				</Center>
 
 				<Center mt={24}>
